d39ef55e0fdcdc1bed3a774d719d1efcbe0989c3
as314159 <aks3141592@gmail.com>
2017-02-12 12:39:54 -0600

    Update HammingDistance.java
    remove commented code

--- HammingDistance.java
+++ HammingDistance.java
@@ -5,24 +5,6 @@ public class Solution {
         }
         StringBuilder sbx = null;
         StringBuilder sby = null;
-        // while (x > 0) {
-        //     int modx = x % 2;
-        //     if (modx == 1) {
-        //         sbx.append("1");
-        //     } else {
-        //         sbx.append("0");
-        //     }
-        //     x = x / 2;
-        // }
-        // while (y > 0) {
-        //     int mody = y % 2;
-        //     if (mody == 1) {
-        //         sby.append("1");
-        //     } else {
-        //         sby.append("0");
-        //     }
-        //     y = y / 2;
-        // }
         sbx = new StringBuilder(Integer.toBinaryString(x));
         sby = new StringBuilder(Integer.toBinaryString(y));
         int lensbx = sbx.length();
