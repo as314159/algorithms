d30af5b70aeac0cd5a872f82135ad776878dd552
as314159 <as314159@users.noreply.github.com>
2017-02-15 18:09:17 -0600

    Update ReverseString.java
    readability - return emptystr var

--- ReverseString.java
+++ ReverseString.java
@@ -1,9 +1,10 @@
 public class ReverseString {
     public String reverseString(String s) {
+        String emptystr = "";
         if (s == null) {
             return null;
         } else if (s.isEmpty()) {
-            return "";
+            return emptystr;
         }
         StringBuilder sb = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--) {
