ee504c029416db79860e12d133ff394f968a7991
as314159 <as314159@users.noreply.github.com>
2017-02-16 18:39:00 -0600

    Update NextGreaterElement.java
    camel casing of vars

--- NextGreaterElement.java
+++ NextGreaterElement.java
@@ -2,29 +2,29 @@ public class NextGreaterElement {
     public int[] nextGreaterElement(int[] findNums, int[] nums) {
         int findnumslen = findNums.length;
         int numslen = nums.length;
-        int[] nextgreatestints = new int[findnumslen];
+        int[] nextGreatestElements = new int[findnumslen];
         for (int i = 0; i < findnumslen; i++) {
             for (int j = 0; j < numslen; j++) {
                 if (findNums[i] == nums[j]) {
                     if (j == numslen - 1) {
-                        nextgreatestints[i] = -1;
+                        nextGreatestElements[i] = -1;
                     } else {
                         int k = j;
                         boolean nextgreatest = false;
                         while (k++ != numslen - 1) {
                             if (findNums[i] < nums[k]) {
-                                nextgreatestints[i] = nums[k];
+                                nextGreatestElements[i] = nums[k];
                                 nextgreatest = true;
                                 break;
                             }
                         }
                         if (!nextgreatest) {
-                            nextgreatestints[i] = -1;
+                            nextGreatestElements[i] = -1;
                         }
                     }
                 }
             }
         }
-        return nextgreatestints;
+        return nextGreatestElements;
     }
 }
